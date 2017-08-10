1aa134deeab5ff6992571ca6b8fd8914e03b2c4a
Petr D. Svoboda <petrdsvoboda@gmail.com>
2017-08-10 10:44:18 +0200

    Setup project

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 
+import 'typeface-roboto'
+
 render(
   	<App />,
   	document.getElementById('root')
