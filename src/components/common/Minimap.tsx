1d37183770f5e22d94738ecd6e7414fc24f339ec
Petr D. Svoboda <petrdsvoboda@gmail.com>
2017-09-11 17:58:54 +0200

    Change color

--- src/components/common/Minimap.tsx
+++ src/components/common/Minimap.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 import { Stage, Layer, Rect } from 'react-konva'
 
-import { blueGrey } from 'material-ui/colors'
+import { grey } from 'material-ui/colors'
 
 interface Props {
 	width?: number,
@@ -35,14 +35,14 @@ const Minimap: React.SFC<Props> = (props: Props): JSX.Element | null => {
 					y={0}
 					width={4 * props.scale + 2}
 					height={props.height * props.scale + 2}
-					fill={props.side === 'left' ? blueGrey[300] : blueGrey[100]}
+					fill={props.side === 'left' ? grey[400] : grey[300]}
 				/>
 				<Rect
 					x={0 + 1}
 					y={0 + 1}
 					width={4 * props.scale}
 					height={props.height * props.scale}
-					fill={props.side === 'left' ? blueGrey[200] : blueGrey[50]}
+					fill={props.side === 'left' ? grey[300] : grey[200]}
 				/>
 
 				{/* Right side */}
@@ -51,14 +51,14 @@ const Minimap: React.SFC<Props> = (props: Props): JSX.Element | null => {
 					y={0}
 					width={4 * props.scale + 2}
 					height={props.height * props.scale + 2}
-					fill={props.side === 'right' ? blueGrey[300] : blueGrey[100]}
+					fill={props.side === 'right' ? grey[400] : grey[300]}
 				/>
 				<Rect
 					x={6 * props.scale + 3}
 					y={0 + 1}
 					width={4 * props.scale}
 					height={props.height * props.scale}
-					fill={props.side === 'right' ? blueGrey[200] : blueGrey[50]}
+					fill={props.side === 'right' ? grey[300] : grey[200]}
 				/>
 
 				{/* Full side */}
@@ -67,14 +67,14 @@ const Minimap: React.SFC<Props> = (props: Props): JSX.Element | null => {
 					y={0}
 					width={71 * props.scale + 2}
 					height={16 * props.scale + 2}
-					fill={blueGrey[100]}
+					fill={grey[300]}
 				/>
 				<Rect
 					x={16 * props.scale + 5}
 					y={0 + 1}
 					width={71 * props.scale}
 					height={16 * props.scale}
-					fill={blueGrey[50]}
+					fill={grey[200]}
 				/>
 
 				{/* Column position */}
@@ -83,7 +83,7 @@ const Minimap: React.SFC<Props> = (props: Props): JSX.Element | null => {
 					y={0}
 					width={props.scale}
 					height={16 * props.scale}
-					fill={blueGrey[100]}
+					fill={grey[300]}
 				/>
 
 				{/* Row position */}
@@ -92,7 +92,7 @@ const Minimap: React.SFC<Props> = (props: Props): JSX.Element | null => {
 					y={2 * props.y * props.scale}
 					width={71 * props.scale}
 					height={2 * props.scale}
-					fill={blueGrey[100]}
+					fill={grey[300]}
 				/>
 
 				{/* Pallet position */}
@@ -101,7 +101,7 @@ const Minimap: React.SFC<Props> = (props: Props): JSX.Element | null => {
 					y={2 * props.y * props.scale}
 					width={props.scale}
 					height={2 * props.scale}
-					fill={blueGrey[300]}
+					fill={grey[400]}
 				/>
 			</Layer>
 		</Stage>
